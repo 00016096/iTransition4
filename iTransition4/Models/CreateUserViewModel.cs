9d4098e1dbf48b1807a345c4e4e8f76c8c809405
00016096 <wiutstudent00016096@gmail.com>
2024-05-01 23:01:12 +0500

    commit

---
+++ iTransition4/Models/CreateUserViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace iTransition4.Models
+{
+    public class CreateUserViewModel
+    {
+        private string fullName;
+        private string email;
+        private string password;
+
+
+        public string FullName
+        {
+            get => fullName; set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new Exception("Full Name cannot be empty!");
+                fullName = value;
+            }
+        }
+        public string Email
+        {
+            get => email; set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new Exception("Email cannot be emoty");
+                email = value;
+            }
+        }
+        public string Password
+        {
+            get => password; set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new Exception("Password cannot be empty");
+                password = value;
+            }
+        }
+        public DateTime LastLoginTime { get; set; }
+        public DateTime RegistrationTime { get; set; }
+        public Status IsBlocked { get; set; } = Status.Active;
+    }
+}
---
+++ iTransition4/Models/User.cs
@@ -0,0 +1,43 @@
+﻿namespace iTransition4.Models
+{
+    public class User
+    {
+        private string fullName;
+        private string email;
+        private string password;
+
+        public Guid Id { get; set; }
+        public string FullName
+        {
+            get => fullName; set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Full Name cannot be empty");
+                }
+                fullName = value;
+            }
+        }
+        public string Email
+        {
+            get => email; set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new Exception("Email cannot be empty");
+                email = value;
+            }
+        }
+        public string Password
+        {
+            get => password; set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new Exception("Password cannot be empty");
+                password = value;
+            }
+        }
+        public DateTime? LastLoginTime { get; set; } = DateTime.MinValue;
+        public DateTime RegistrationTime { get; set; }
+        public Status IsBlocked { get; set; } = Status.Active;
+    }
+}
